4092b5c5fa00bcfa2bd3a7d780a014397f923def
Wakaba <wakaba@lemon>
2012-04-22 13:53:09 +0900

    Custom HTML-like document parser

---
+++ lib/Temma/Parser.pm
@@ -0,0 +1,575 @@
+package Temma::Parser;
+use strict;
+use warnings;
+no warnings 'utf8';
+our $VERSION = '2.1';
+
+push our @ISA, 'Whatpm::HTML';
+use Whatpm::HTML::Tokenizer qw/:token/;
+
+use constant HTML_NS => q<http://www.w3.org/1999/xhtml>;
+use constant TEMMA_NS => q<http://suika.fam.cx/www/markup/temma>;
+
+sub parse_char_string ($$$;$$) {
+  #my ($self, $s, $doc, $onerror, $get_wrapper) = @_;
+  my $self = shift;
+  my $s = ref $_[0] ? $_[0] : \($_[0]);
+  require Whatpm::Charset::DecodeHandle;
+  my $input = Whatpm::Charset::DecodeHandle::CharString->new ($s);
+  return $self->parse_char_stream ($input, @_[1..$#_]);
+} # parse_char_string
+
+sub parse_char_stream ($$$;$$) {
+  my $self = ref $_[0] ? shift : shift->new;
+  my $input = $_[0];
+  my $doc = $self->{document} = $_[1];
+  @{$self->{document}->child_nodes} = ();
+
+  ## NOTE: |set_inner_html| copies most of this method's code
+
+  $self->{confident} = 1 unless exists $self->{confident};
+  $self->{document}->input_encoding ($self->{input_encoding})
+      if defined $self->{input_encoding};
+## TODO: |{input_encoding}| is needless?
+
+  $self->{line_prev} = $self->{line} = 1;
+  $self->{column_prev} = -1;
+  $self->{column} = 0;
+  $self->{set_nc} = sub {
+    my $self = shift;
+
+    my $char = '';
+    if (defined $self->{next_nc}) {
+      $char = $self->{next_nc};
+      delete $self->{next_nc};
+      $self->{nc} = ord $char;
+    } else {
+      $self->{char_buffer} = '';
+      $self->{char_buffer_pos} = 0;
+
+      my $count = $input->manakai_read_until
+         ($self->{char_buffer}, qr/[^\x0A\x0D]/, $self->{char_buffer_pos});
+      if ($count) {
+        $self->{line_prev} = $self->{line};
+        $self->{column_prev} = $self->{column};
+        $self->{column}++;
+        $self->{nc}
+            = ord substr ($self->{char_buffer}, $self->{char_buffer_pos}++, 1);
+        return;
+      }
+
+      if ($input->read ($char, 1)) {
+        $self->{nc} = ord $char;
+      } else {
+        $self->{nc} = -1;
+        return;
+      }
+    }
+
+    ($self->{line_prev}, $self->{column_prev})
+        = ($self->{line}, $self->{column});
+    $self->{column}++;
+    
+    if ($self->{nc} == 0x000A) { # LF
+      
+      $self->{line}++;
+      $self->{column} = 0;
+    } elsif ($self->{nc} == 0x000D) { # CR
+      
+## TODO: support for abort/streaming
+      my $next = '';
+      if ($input->read ($next, 1) and $next ne "\x0A") {
+        $self->{next_nc} = $next;
+      }
+      $self->{nc} = 0x000A; # LF # MUST
+      $self->{line}++;
+      $self->{column} = 0;
+    }
+  };
+
+  $self->{read_until} = sub {
+    #my ($scalar, $specials_range, $offset) = @_;
+    return 0 if defined $self->{next_nc};
+
+    my $pattern = qr/[^$_[1]\x00\x0A\x0D\x{D800}-\x{DFFF}]/;
+    my $offset = $_[2] || 0;
+
+    if ($self->{char_buffer_pos} < length $self->{char_buffer}) {
+      pos ($self->{char_buffer}) = $self->{char_buffer_pos};
+      if ($self->{char_buffer} =~ /\G(?>$pattern)+/) {
+        substr ($_[0], $offset)
+            = substr ($self->{char_buffer}, $-[0], $+[0] - $-[0]);
+        my $count = $+[0] - $-[0];
+        if ($count) {
+          $self->{column} += $count;
+          $self->{char_buffer_pos} += $count;
+          $self->{line_prev} = $self->{line};
+          $self->{column_prev} = $self->{column} - 1;
+          $self->{nc} = -1;
+        }
+        return $count;
+      } else {
+        return 0;
+      }
+    } else {
+      my $count = $input->manakai_read_until ($_[0], $pattern, $_[2]);
+      if ($count) {
+        $self->{column} += $count;
+        $self->{line_prev} = $self->{line};
+        $self->{column_prev} = $self->{column} - 1;
+        $self->{nc} = -1;
+      }
+      return $count;
+    }
+  }; # $self->{read_until}
+
+  my $onerror = $_[2] || sub {
+    my (%opt) = @_;
+    my $line = $opt{token} ? $opt{token}->{line} : $opt{line};
+    my $column = $opt{token} ? $opt{token}->{column} : $opt{column};
+    warn "Parse error ($opt{type}) at line $line column $column\n";
+  };
+  $self->{parse_error} = sub {
+    $onerror->(line => $self->{line}, column => $self->{column}, @_);
+  };
+
+  my $char_onerror = sub {
+    my (undef, $type, %opt) = @_;
+    $self->{parse_error}->(level => $self->{level}->{must}, layer => 'encode',
+                    line => $self->{line}, column => $self->{column} + 1,
+                    %opt, type => $type);
+  }; # $char_onerror
+
+  if ($_[3]) {
+    $input = $_[3]->($input);
+    $input->onerror ($char_onerror);
+  } else {
+    $input->onerror ($char_onerror) unless defined $input->onerror;
+  }
+
+  $self->_initialize_tokenizer;
+  $self->_initialize_tree_constructor;
+  $self->_construct_tree;
+  $self->_terminate_tree_constructor;
+  
+  ## Remove self references.
+  delete $self->{set_nc};
+  delete $self->{read_until};
+  delete $self->{parse_error};
+  delete $self->{document};
+
+  return $doc;
+} # parse_char_stream
+
+sub new ($) {
+  my $class = shift;
+  my $self = bless {
+    level => {must => 'm',
+              should => 's',
+              warn => 'w',
+              info => 'i',
+              uncertain => 'u'},
+  }, $class;
+  $self->{set_nc} = sub {
+    $self->{nc} = -1;
+  };
+  $self->{parse_error} = sub {
+    # 
+  };
+  $self->{change_encoding} = sub { };
+  $self->{application_cache_selection} = sub { };
+
+  return $self;
+} # new
+
+sub _initialize_tree_constructor ($) {
+  my $self = shift;
+  ## NOTE: $self->{document} MUST be specified before this method is called
+  $self->{document}->strict_error_checking (0);
+  ## TODO: Turn mutation events off # MUST
+  $self->{document}->manakai_is_html (1);
+  $self->{document}->set_user_data (manakai_source_line => 1);
+  $self->{document}->set_user_data (manakai_source_column => 1);
+} # _initialize_tree_constructor
+
+sub _terminate_tree_constructor ($) {
+  my $self = shift;
+  $self->{document}->strict_error_checking (1);
+  ## TODO: Turn mutation events on
+} # _terminate_tree_constructor
+
+## Tree construction stage
+
+my @metavoid = qw(link meta base basefont bgsound command);
+my @metacontent = qw(title style script);
+my @bodyvoid = qw(
+  area br embed img keygen wbr input param source track hr image isindex
+  col
+);
+
+our $AutoOpen = {
+  'html' => {
+    '<text>' => 'body',
+    'head' => '',
+    'body' => '',
+    '<start>' => 'body',
+    (map { $_ => 'head' } @metavoid),
+    (map { $_ => 'head' } @metacontent),
+  },
+  'table' => {
+    'tr' => 'tbody',
+  },
+};
+
+our $AutoClose = {
+  head => {
+    '<text>' => 1,
+    '<start>' => 1,
+    'head' => '',
+    (map { $_ => '' } @metavoid),
+    (map { $_ => '' } @metacontent),
+  },
+  tr => {
+    thead => 1,
+    tbody => 1,
+    tfoot => 1,
+  },
+  th => {
+    thead => 1,
+    tbody => 1,
+    tfoot => 1,
+  },
+  td => {
+    thead => 1,
+    tbody => 1,
+    tfoot => 1,
+  },
+  colgroup => {
+    thead => 1,
+    tbody => 1,
+    tfoot => 1,
+    colgroup => 1,
+    tr => 1,
+  },
+  option => {
+    option => 1,
+    optgroup => 1,
+  },
+  optgroup => {
+    optgroup => 1,
+  },
+  rt => {
+    rp => 1,
+    rt => 1,
+  },
+  rp => {
+    rp => 1,
+    rt => 1,
+  },
+  't:else' => {
+    '<text>' => 1,
+    '<start>' => 1,
+  },
+};
+
+our $Void = {
+  (map { $_ => 1 } @metavoid),
+  (map { $_ => 1 } @bodyvoid),
+};
+
+our $RawContent = {
+  script => Whatpm::HTML::SCRIPT_DATA_STATE,
+  style => Whatpm::HTML::RAWTEXT_STATE,
+  textarea => Whatpm::HTML::RCDATA_STATE,
+};
+
+our $CloseIfInScope = {
+  (map { $_ => {p => 1, body => -1} } qw(
+    address article aside blockquote center details dialog dir div dl
+    fieldset figcaption figure footer header hgroup menu nav ol
+    section summary ul h1 h2 h3 h4 h5 h6 pre listing form plaintext
+    form table hr isindex xmp
+  )),
+  p => {p => 1, body => -1},
+  li => {li => 1, ol => -1, ul => -1, menu => -1, dir => -1, body => -1},
+  dt => {dt => 1, dd => 1, dl => -1, body => -1},
+  dd => {dt => 1, dd => 1, dl => -1, body => -1},
+  'thead' => {'table' => 0},
+  'tbody' => {'table' => 0},
+  'tfoot' => {'table' => 0},
+  'tr' => {'tr' => 1, 'table' => -1},
+  'th' => {'th' => 1, 'td' => 1, 'table' => -1},
+  'td' => {'th' => 1, 'td' => 1, 'table' => -1},
+  't:else' => {'t:if' => 0},
+};
+
+our $EndTagOptional = {
+  p => 1,
+  li => 1,
+  dt => 1,
+  dd => 1,
+  tr => 1,
+  th => 1,
+  td => 1,
+  thead => 1,
+  tbody => 1,
+  tfoot => 1,
+  rt => 1,
+  rp => 1,
+  option => 1,
+  optgroup => 1,
+};
+
+sub _construct_tree ($) {
+  my ($self) = @_;
+
+  delete $self->{tainted};
+  $self->{open_elements} = [];
+
+  my $doctype = $self->{document}
+      ->implementation->create_document_type ('html');
+  $self->{document}->append_child ($doctype);
+
+  my $el = $self->{document}->create_element_ns (HTML_NS, [undef, 'html']);
+  $self->{document}->append_child ($el);
+  push @{$self->{open_elements}}, [$el, 'html'];
+
+  $self->{t} = $self->_get_next_token;
+
+  B: while (1) {
+    if ($self->{t}->{type} == CHARACTER_TOKEN) {
+      if ($self->{t}->{data} =~ s/^([\x09\x0A\x0C\x0D\x20]+)//) {
+        $self->{open_elements}->[-1]->[0]->manakai_append_text ($1);
+      }
+
+      if (not length $self->{t}->{data}) {
+        $self->{t} = $self->_get_next_token;
+        next B;
+      }
+
+      if ($Void->{$self->{open_elements}->[-1]->[1]}) {
+        pop @{$self->{open_elements}};
+      }
+
+      while (1) {
+        my $ac = $AutoClose->{$self->{open_elements}->[-1]->[1]}->{'<text>'};
+        if ($ac) {
+          pop @{$self->{open_elements}};
+        } else {
+          last;
+        }
+      }
+      
+      while (1) {
+        my $ao = $AutoOpen->{$self->{open_elements}->[-1]->[1]}->{'<text>'};
+        if ($ao) {
+          my $el = $self->{document}->create_element_ns
+              (HTML_NS, [undef, $ao]);
+          $self->{open_elements}->[-1]->[0]->append_child ($el);
+          push @{$self->{open_elements}}, [$el, $ao];
+        } else {
+          last;
+        }
+      }
+
+      while ($self->{t}->{data} =~ s/\x00/\x{FFFD}/) {
+        $self->{parse_error}->(level => $self->{level}->{must}, type => 'NULL', token => $self->{t});
+      }
+      $self->{open_elements}->[-1]->[0]->manakai_append_text ($self->{t}->{data});
+      
+      $self->{t} = $self->_get_next_token;
+      next B;
+    } elsif ($self->{t}->{type} == START_TAG_TOKEN) {
+      my $tag_name = $self->{t}->{tag_name};
+      $tag_name =~ tr/A-Z/a-z/; ## ASCII case-insensitive.
+
+      if ($tag_name eq 'html' or
+          $tag_name eq 'head' or
+          $tag_name eq 'body') {
+        if (@{$self->{open_elements}} > 1 and
+            not (@{$self->{open_elements}} == 2 and
+                 $self->{open_elements}->[1]->[1] eq 'head' and
+                 $tag_name eq 'body')) {
+          $self->{parse_error}->(level => $self->{level}->{must},
+                                 type => 'start_tag_not_allowed',
+                                 text => $tag_name,
+                                 token => $self->{t});
+
+          delete $self->{self_closing};
+          $self->{t} = $self->_get_next_token;
+          next B;
+        } elsif ($tag_name eq 'html') {
+          my $el = $self->{open_elements}->[0]->[0];
+          if ($el) {
+            my $attrs = $self->{t}->{attributes};
+            for my $attr_name (keys %{$attrs}) {
+              next if $el->has_attribute_ns (undef, $attr_name);
+              my $attr_t = $attrs->{$attr_name};
+              my $attr = $self->{document}->create_attribute_ns
+                  (undef, [undef, $attr_name]);
+              $attr->value ($attr_t->{value});
+              $attr->set_user_data (manakai_source_line => $attr_t->{line});
+              $attr->set_user_data (manakai_source_column => $attr_t->{column});
+              $el->set_attribute_node_ns ($attr);
+            }
+          }
+
+          delete $self->{self_closing};
+          $self->{t} = $self->_get_next_token;
+          next B;
+        }
+      }
+
+      if (not $tag_name =~ /^t:/ and
+          $Void->{$self->{open_elements}->[-1]->[1]}) {
+        pop @{$self->{open_elements}};
+      }
+
+      while (1) {
+        my $ac = $AutoClose->{$self->{open_elements}->[-1]->[1]}->{$tag_name};
+        $ac = $AutoClose->{$self->{open_elements}->[-1]->[1]}->{'<start>'}
+            if not defined $ac;
+        if ($ac) {
+          pop @{$self->{open_elements}};
+        } else {
+          last;
+        }
+      }
+
+      while (1) {
+        my $ao = $AutoOpen->{$self->{open_elements}->[-1]->[1]}->{$tag_name};
+        $ao = $AutoOpen->{$self->{open_elements}->[-1]->[1]}->{'<start>'}
+            if not defined $ao;
+        if ($ao) {
+          my $el = $self->{document}->create_element_ns
+              (HTML_NS, [undef, $ao]);
+          $self->{open_elements}->[-1]->[0]->append_child ($el);
+          push @{$self->{open_elements}}, [$el, $ao];
+        } else {
+          last;
+        }
+      }
+
+      my $cis = $CloseIfInScope->{$tag_name};
+      if ($cis) {
+        my $i = 0;
+        for (reverse @{$self->{open_elements}}) {
+          my $diff = $cis->{$_->[1]};
+          if (defined $diff) {
+            last if $diff < 0;
+            my @closed = splice @{$self->{open_elements}},
+                -($i + $diff), $i + $diff => ();
+            shift @closed;
+            my @not_closed = grep { not $EndTagOptional->{$_->[1]} } @closed;
+            if (@not_closed) {
+              $self->{parse_error}->(level => $self->{level}->{must},
+                                     type => 'not closed',
+                                     text => $not_closed[-1]->[1],
+                                     token => $self->{t});
+            }
+            last;
+          }
+          $i++;
+        }
+      }
+
+      my $ns = HTML_NS;
+      my $local_name = $tag_name;
+
+      if ($local_name =~ s/^t://) {
+        $ns = TEMMA_NS;
+      }
+
+      # XXX SVG, MathML
+      my $el = $self->{document}->create_element_ns ($ns, [undef, $local_name]);
+      $el->set_user_data (manakai_source_line => $self->{t}->{line});
+      $el->set_user_data (manakai_source_column => $self->{t}->{column});
+
+      my $attrs = $self->{t}->{attributes};
+      for my $attr_name (sort {$attrs->{$a}->{index} <=> $attrs->{$b}->{index}}
+                         keys %{$attrs}) {
+        my $attr_t = $attrs->{$attr_name};
+        my $attr = $self->{document}->create_attribute_ns
+            (undef, [undef, $attr_name]);
+        $attr->value ($attr_t->{value});
+        $attr->set_user_data (manakai_source_line => $attr_t->{line});
+        $attr->set_user_data (manakai_source_column => $attr_t->{column});
+        $el->set_attribute_node_ns ($attr);
+      }
+
+      $self->{open_elements}->[-1]->[0]->append_child ($el);
+
+      if ($self->{self_closing}) {
+        delete $self->{self_closing};
+      } else {
+        push @{$self->{open_elements}}, [$el, $tag_name];
+
+        if ($RawContent->{$tag_name}) {
+          $self->{state} = $RawContent->{$tag_name};
+          delete $self->{escape};
+        }
+      }
+      
+      $self->{t} = $self->_get_next_token;
+      next B;
+
+    } elsif ($self->{t}->{type} == END_TAG_TOKEN) {
+      my $tag_name = $self->{t}->{tag_name};
+      $tag_name =~ tr/A-Z/a-z/; ## ASCII case-insensitive.
+
+      if ($tag_name eq 'html' or $tag_name eq 'body') {
+        #
+      } elsif ($tag_name eq 'sarcasm') {
+        my $sarcasm = $self->{document}->create_element_ns
+            (HTML_NS, [undef, 'sarcasm']);
+        $self->{open_elements}->[-1]->[0]->append_child ($sarcasm);
+      } elsif ($self->{open_elements}->[-1]->[1] eq $tag_name) {
+        pop @{$self->{open_elements}};
+      } else {
+        ## Has an element in scope
+        INSCOPE: {
+          for my $i (reverse 0..$#{$self->{open_elements}}) {
+            if ($self->{open_elements}->[$i]->[1] eq $tag_name) {
+              my @closed = splice @{$self->{open_elements}}, $i;
+              shift @closed;
+              @closed = grep { not $EndTagOptional->{$_->[1]} } reverse @closed;
+              if (@closed) {
+                $self->{parse_error}->(level => $self->{level}->{must},
+                                       type => 'not closed',
+                                       text => $closed[-1]->[1],
+                                       token => $self->{t});
+              }
+              last INSCOPE;
+            }
+          }
+
+          $self->{parse_error}->(level => $self->{level}->{must},
+                                 type => 'unmatched end tag',
+                                 text => $self->{t}->{tag_name},
+                                 token => $self->{t});
+        } # INSCOPE
+      }
+      
+      $self->{t} = $self->_get_next_token;
+      redo B;
+    } elsif ($self->{t}->{type} == COMMENT_TOKEN) {
+      my $comment = $self->{document}->create_comment ($self->{t}->{data});
+      $self->{open_elements}->[-1]->[0]->append_child ($comment);
+      
+      $self->{t} = $self->_get_next_token;
+      next B;
+    } elsif ($self->{t}->{type} == END_OF_FILE_TOKEN) {
+      $self->{t} = {type => ABORT_TOKEN};
+      return;
+    } elsif ($self->{t}->{type} == DOCTYPE_TOKEN) {
+      $self->{t} = $self->_get_next_token;
+      next B;
+    } elsif ($self->{t}->{type} == ABORT_TOKEN) {
+      return;
+    } else {
+      die "Unknown token type $self->{t}->{type}";
+    }
+  } # B
+} # _tree_in_element
+
+1;
---
+++ t/parser.t
@@ -0,0 +1,60 @@
+package test::Temma::Parser;
+use strict;
+use warnings;
+use Path::Class;
+use lib file (__FILE__)->dir->parent->subdir ('lib')->stringify;
+use lib glob file (__FILE__)->dir->parent->subdir ('modules', '*', 'lib')->stringify;
+use Test::MoreMore;
+use Test::HTCT::Parser;
+use Temma::Parser;
+use Message::DOM::DOMImplementation;
+use Whatpm::HTML::Dumper;
+use base qw(Test::Class);
+
+my $test_data_d = file (__FILE__)->dir->subdir ('data');
+
+sub _parsed_tree : Tests {
+  my $dom = Message::DOM::DOMImplementation->new;
+
+  for_each_test $_->stringify, {
+    data => {is_prefixed => 1},
+    errors => {is_list => 1},
+    document => {is_prefixed => 1},
+  }, sub {
+    my $test = shift;
+
+    my @error;
+    my $parser = Temma::Parser->new;
+    my $onerror = sub {
+      my %opt = @_;
+      push @error, join ';', map { 
+        defined $_ ? $_ : '';
+      } @opt{qw(line column level type value text)};
+    }; # onerror
+
+    my $doc = $dom->create_document;
+
+    $parser->parse_char_string ($test->{data}->[0] => $doc, $onerror);
+
+    my $actual = dumptree $doc;
+    $actual =~ s/\n$//;
+    eq_or_diff $actual, $test->{document}->[0];
+
+    eq_or_diff [sort { $a cmp $b } @error],
+        [sort { $a cmp $b } @{$test->{errors}->[0]}];
+  } for map { $test_data_d->file ($_) } qw(
+    html-1.dat
+    html-meta-1.dat
+    html-void-1.dat
+    html-flow-1.dat
+    html-lists-1.dat
+    html-tables-1.dat
+    html-forms-1.dat
+    html-text-1.dat
+  );
+} # _parsed_tree
+
+__PACKAGE__->runtests;
+
+1;
+
